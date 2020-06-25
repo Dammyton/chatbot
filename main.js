832066d516e4ed2dba904a18732e33fac67b5021
Damilola Owolabi <44869245+Dammyton@users.noreply.github.com>
2020-06-25 15:32:05 +0100

    Updated

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ const coronavirus = ["Please stay home"];
 document.addEventListener("DOMContentLoaded", () => {
 	const inputField = document.getElementById("input")
 	inputField.addEventListener("keydown", function(e) {
-		if (e.code === "Tab") {
+		if (e.code === "Enter") {
 			let input = inputField.value;
 			inputField.value = "";
 			output(input);
